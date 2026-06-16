d4a77b487c24abc3d5418b11b2a8e86b4f05a933
Calyx Hikari <29157608+HikariCalyx@users.noreply.github.com>
2026-06-16 15:50:59 +0800

    Update build.rs

--- tango-signaling/build.rs
+++ tango-signaling/build.rs
@@ -1,6 +1,8 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    prost_build::compile_protos(&["src/proto/signaling.proto"], &["src/proto"])?;
+    let mut config = prost_build::Config::new();
+    config.protoc_arg("--experimental_allow_proto3_optional");
+    config.compile_protos(&["src/proto/signaling.proto"], &["src/proto"])?;
     Ok(())
 }
