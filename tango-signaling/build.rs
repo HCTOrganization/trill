c3a6c39d449f048cabdabb6e4cd66c40547c311c
Calyx Hikari <29157608+HikariCalyx@users.noreply.github.com>
2026-06-17 14:43:26 +0800

    Fix missing dependency

--- tango-signaling/build.rs
+++ tango-signaling/build.rs
@@ -1,8 +1,6 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    let mut config = prost_build::Config::new();
-    config.protoc_arg("--experimental_allow_proto3_optional");
-    config.compile_protos(&["src/proto/signaling.proto"], &["src/proto"])?;
+    prost_build::compile_protos(&["src/proto/signaling.proto"], &["src/proto"])?;
     Ok(())
 }
